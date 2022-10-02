3160a0efb9157903ba49a171264de56cd72490eb
premsai <143premsai@gmail.com>
2022-10-02 18:12:29 -0400

    Added Rectangle Test Class

---
+++ Assignment02Tests/RectangleTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment02;
+using NUnit.Framework;
+
+namespace Assignment02Tests
+{
+    [TestFixture]
+    public class RectangleTest
+    {
+        
+    }
+}
