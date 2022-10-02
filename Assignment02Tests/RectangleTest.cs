d5165fc1011c0d309ab512e51159589da3d2e219
premsai <143premsai@gmail.com>
2022-10-02 18:33:07 -0400

    Updated 2 Unit Test Cases for GetCurrentLength Method

--- Assignment02Tests/RectangleTest.cs
+++ Assignment02Tests/RectangleTest.cs
@@ -11,6 +11,34 @@ namespace Assignment02Tests
     [TestFixture]
     public class RectangleTest
     {
+        [Test]
+        public void Valid_GetRectangleLength_NoInput_OutputDefaultValues()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int expectedLength = 1;
+
+            // Act
+            int actualLength = rect.GetCurrentLength();
+
+            // Assert
+            Assert.AreEqual(expectedLength, actualLength);
+        }
+
+        [Test]
+        public void Valid_GetRectangleLength_InputLength20_OutputLength20()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle(20, 20);
+            int expectedLength = 20;
+
+            // Act
+            int actualLength = rect.GetCurrentLength();
+
+            // Assert
+            Assert.AreEqual(expectedLength, actualLength);
+        }
+
         
     }
 }
